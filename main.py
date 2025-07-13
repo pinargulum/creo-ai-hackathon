bcfb83b56b6656d1be3314f8bcf28abc1a262fde
unknown <pinargulum@hotmail.com>
2025-07-13 11:17:36 -0400

    main.py updated

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 from routes.chat import router as chat_router
 from routes.upload import router as upload_router
 from routes.crm import router as crm
+from dotenv import load_dotenv
 
-
+load_dotenv()
 app = FastAPI()
 app.include_router(chat_router)
 app.include_router(upload_router)
